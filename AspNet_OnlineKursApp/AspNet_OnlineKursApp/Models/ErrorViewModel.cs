096320348d9b19ec6bf7983c7705c90ea6c13beb
burakcirak <burakcirak67@gmail.com>
2024-02-12 10:22:29 +0300

    #Başlangıç Commiti

---
+++ AspNet_OnlineKursApp/AspNet_OnlineKursApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNet_OnlineKursApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
