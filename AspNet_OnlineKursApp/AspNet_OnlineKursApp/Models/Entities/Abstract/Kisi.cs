0b82642bbf716a6812b3c20e121697d54be46e24
burakcirak <burakcirak67@gmail.com>
2024-02-12 12:29:39 +0300

    BaseEntityOluşturuldu.

---
+++ AspNet_OnlineKursApp/AspNet_OnlineKursApp/Models/Entities/Abstract/Kisi.cs
@@ -0,0 +1,19 @@
+﻿namespace AspNet_OnlineKursApp.Models.Entities.Abstract
+{
+    public abstract class Kisi:BaseEntity
+    {
+        public Kisi()
+        {
+               
+        }
+        public string Ad { get; set; }
+        public string SoyAd { get; set; }
+        public string TcKimlikNo { get; set; }
+        public string Email { get; set; }
+        public string Telefon { get; set; }
+
+
+
+
+    }
+}
---
+++ AspNet_OnlineKursApp/AspNet_OnlineKursApp/Models/Entities/Concreate/Egitmen.cs
@@ -0,0 +1,10 @@
+﻿using AspNet_OnlineKursApp.Models.Entities.Abstract;
+
+namespace AspNet_OnlineKursApp.Models.Entities.Concreate
+{
+    public class Egitmen:Kisi
+    {
+        public DateTime ISeBaslamaTarihi { get; set; }
+        public DateTime IstenAyrilmaTarihi { get; set; }
+    }
+}
